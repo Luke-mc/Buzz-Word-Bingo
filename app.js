a8eb85e2fc00e75f5e904622e223d404f73f7696
Luke-mc <luke_mcglynn@yahoo.com>
2017-07-13 19:25:34 -1000

    PUT done

--- app.js
+++ app.js
@@ -4,14 +4,8 @@ const app = express();
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use (bodyParser.json());
-//const router = express.Router();
-// const foodRoutes = require("./routers/foods.js");
-// const dogRoutes = require("./routers/dogs.js");
 const buzzWordsArray = [];
 const objectArray = [];
-
-
-
 var html = app.use(express.static('public/'));
 
 
@@ -22,13 +16,12 @@ app.get("/", (req, res)=>{
 app.post("/buzzword", (req, res)=>{
   if(buzzWordsArray.indexOf(req.body.buzzword) !== -1){
     res.send({"success": false});
-
   }else{
+    req.body.heard = false;
     objectArray.push(req.body);
     buzzWordsArray.push(req.body.buzzword);
     res.send({"success": true});
    }
-
 });
 
 app.get("/buzzword", (req, res)=>{
@@ -37,17 +30,34 @@ app.get("/buzzword", (req, res)=>{
   console.log(objectArray);
 });
 
-
 app.put('/buzzword', (req, res)=>{
-for(var i = 0; i < objectArray.length; i++){
+  for(var i = 0; i < objectArray.length; i++){
+      if(objectArray[i].buzzword === req.body.buzzword){
+        var currentNum = parseInt(objectArray[i].points);
+        var newNum = parseInt(req.body.points);
+        var number = currentNum += newNum;
 
-  if(objectArray[i].buzzword === req.body.buzzword){
-    console.log(objectArray[i].buzzword);
-   res.send({"success": true});
-  }else{
-    res.send({"succes": false});
-  }
- }
+        res.send({"success": true, newScore: number, heard: objectArray[i].heard});
+        //res.end();
+      }else{
+         res.send({"succes": false});
+      }
+   }
+});
+
+app.delete('/buzzword', (req, res)=>{
+  for(var i = 0; i < objectArray.length; i++){
+      if(objectArray[i].buzzword === req.body.buzzword){
+        var currentNum = parseInt(objectArray[i].points);
+        var newNum = parseInt(req.body.points);
+        var number = currentNum += newNum;
+        objectArray[i].heard = true;
+        res.send({"success": true, newScore: number});
+        //res.end();
+      }else{
+         res.send({"succes": false});
+      }
+   }
 });
 
 
